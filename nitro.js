a864bc305eae2d34fbe2a44cfe9ce732cff6cad4
spatiokinetic <coupedetat12@gmail.com>
2024-08-25 23:45:08 -0400

    Update nitro.js

--- nitro.js
+++ nitro.js
@@ -1,1 +1,41 @@
+const axios = require('axios');
+const BaseEvent = require('../utils/structures/BaseEvent');
 
+module.exports = class GuildMemberUpdateEvent extends BaseEvent {
+	constructor() {
+	  super('guildMemberUpdate');
+	}
+
+	async run(client, oldMember, newMember) {
+	  const channel = client.channels.cache.get('ID');
+	  
+	  if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
+	    if (!oldMember.roles.cache.has('ROLEID') && newMember.roles.cache.has('ROLEID')) {
+
+	      // Fetch Roblox username using Bloxlink API
+	      const discordID = newMember.id;
+	      try {
+	        const response = await axios.get(`https://v3.blox.link/developer/discord/${discordID}`, {
+	          headers: {
+	            'Authorization': 'your-bloxlink-api-key'
+	          }
+	        });
+
+	        const robloxUsername = response.data?.primaryAccount?.username;
+
+	        if (robloxUsername) {
+	          // Add to the {BOOSTERS} table
+	          BOOSTERS.set(newMember.id, robloxUsername);
+
+	          // Additional code you may want to include
+	          channel.send(`${newMember.user.tag} (${robloxUsername}) has become a booster!`);
+	        } else {
+	          console.log('No Roblox username found for this user.');
+	        }
+	      } catch (error) {
+	        console.error('Error fetching Roblox username:', error);
+	      }
+	    }
+	  }
+	}
+};
